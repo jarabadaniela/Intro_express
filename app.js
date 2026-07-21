678fdd58f28b17623f09d46b5b1c1d97f4faf901
daniela <jarabadaniela97@gmail.com>
2026-07-21 11:56:32 -0500

    finalizado

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-//import express from "express";//ES
-const express = require("express")
+import express from "express";
+//import {configDotenv} from "dotenv"
+//configDotenv();
+import "dotenv/config";
+
+/* const express = require("express") */
 const app = express();
-const puerto = Process.env.PORT || 3000;
+const puerto = process.env.PORT || 3500;
 
 app.get("/",(req,res)=>{
-    res.send("Hola ficha 3407180");     
+    res.send("Hola ficha 3407180 Daniela");     
 });
 
+app.get("/misaludo:ficha",(req,res)=>{
+    const ficha= req.params.ficha;
+    res.send(`<h1>Saludo</h1><p>Hola soy Nani de la ficha ${ficha} </p>`);     
+});
+
+app.get("/clientes/:id",(req,res)=>{
+    const id = req.params.id;
+    res.send(`<h1>Clientes</h1><p>Soy el cliente con ID ${id} </p>`);     
+});
+
+
 app.listen(puerto, ()=>{
-    console.log(`Servidor funcionando!! en el puerto ${spuerto}`);
+    console.log(`SERVIDOR http://localhost:${puerto}
+        http://127.0.0.1:${puerto}`); 
 });
