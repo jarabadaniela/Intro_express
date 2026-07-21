60b6847ffefc692dd2564de1074b6b608eb477c1
Daniela <jarabadaniela97@gmail.com>
2026-07-21 09:59:07 -0500

    Cambio comillas

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 //import express from "express";//ES
 const express = require("express")
 const app = express();
-const puerto = 3000;
+const puerto = Process.env.PORT || 3000;
 
 app.get("/",(req,res)=>{
     res.send("Hola ficha 3407180");     
 });
 
 app.listen(puerto, ()=>{
-    console.log("Servidor funcionando!! ");
+    console.log(`Servidor funcionando!! en el puerto ${spuerto}`);
 });
