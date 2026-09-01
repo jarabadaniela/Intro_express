25c1421e409335857ef330298221c4da263b850c
daniela <jarabadaniela97@gmail.com>
2026-09-01 07:27:30 -0500

    multer

--- app.js
+++ app.js
@@ -1,78 +1,94 @@
-const express = require("express") 
+const express = require("express")
 require("dotenv").config()
-//configuracion
-const app = express();
+const app = express()
+  //configuracion de body-parse
+app.use(express.json())
 const sistemaArchivos = require("fs")
 const ruta = require("path")
-const {json} = require("stream/consumers")
-app.use (express.json())
+const { json } = require("stream/consumers")
+const puerto = process.env.PORT || 3000;
 
-const puerto = process.env.PORT || 3050;
+//importar la libreria multer 
+const multer = require ("multer")
 
-//ruta de mi archivo json
-const rutaArchivosjson = ruta.join(__dirname, "aprendices.json")
+//configurar el lugar almacenamiento
+const almacenamiento = multer.diskStorage({
+    destination:(req, file, cb)=>{
+        cb(null, "misImagenes/")
+    },
+    filename: (req, file, cb)=>{
+        const extension = ruta
+        cb(null, `${Date.now()}`)
+    }
+})
+
+//configurar la accion de carga
+const cargar
+
+  //ruta de mi archivo json
+  const rutaArchivojson = ruta.join(__dirname, "aprendices.json")
 
 app.get("/", (req, res) => {
     res.send("<h1>Api Aprendices</h1>")
 })
 
-// listar aprendices
+//listar aprendices
 
-app.get("/api/aprendices",(req, res) => { 
-    sistemaArchivos.readFile(rutaArchivosjson, "utf-8", (error, datos) => 
+app.get("/api/aprendices", (req, res)=> {
+    sistemaArchivos.readFile(rutaArchivojson, "utf-8", (error, datos) =>
     {
         if (error) {
             return res.status(500).json({ Error: "Error conexion bd."})
         }
-        const listaAprendices = JSON.parse(datos) 
+        const listaAprendices = JSON.parse(datos)
         res.json(listaAprendices)
     })
 })
-
 
 //endpoint para adicionar
 app.post("/api/aprendices", (req, res) => {
-    //capturar los datos enviados 
-    const datosAprendiz = req.body 
-    sistemaArchivos.readFile(rutaArchivosjson, "utf-8", (error, datos) => 
+    //capturar los datos enviados
+    const datosAprendiz = req.body
+    sistemaArchivos.readFile(rutaArchivojson, "utf-8", (error, datos) =>
     {
         if (error) {
-            return res.status(500).json({Error: "Error conexion bd."})
-        } 
+            return res.status(500).json({ Error: "Error conexion bd."})
+        }
         const listaAprendices = JSON.parse(datos)
-        //agregar a la lista javaScript
-    listaAprendices.push(datosAprendiz)
+        //agregar a la lista javascrpit    
+        listaAprendices.push(datosAprendiz)
         //escritura de archivo
-        sistemaArchivos.writeFile(rutaArchivosjson, JSON.stringify(listaAprendices, null, 2), (error) => {
+        sistemaArchivos.writeFile(rutaArchivojson, JSON.stringify(listaAprendices, null, 2), (error) => {
             if (error) {
-                return res.json({Error: "No se puede registrar."})
+                return res.json({ Error: "No se puede registrar." })
             }
             res.status(201).json(datosAprendiz)
         })
-        res.json(listaAprendices)
+        res.json(datosAprendiz)
     })
-    
 })
 
-//endpoint para editar 
-app.put("api/aprendices/:di", (req, res) => {
+//endpoint para editar
+app.put("api/aprendices/:di", (req, res) =>{
     const diAprendiz = req.params
-    const datosAprendiz = req.body
+    const datosAprendiz= req.body
     sistemaArchivos.readFile(rutaArchivojson, "utf-8", (error, datos) => {
         if (error) {
-            return res.status(500).json({Error: "Error"})
+            return res.status(500).json({ Error: "Error de conexion bd."})
         }
+        const listaAprendices = JSON.parse(datos)
+          //Actualizar aprendiz
+        listaAprendices = listaAprendices.map(aprendiz =>{
+            return aprendiz.di === diAprendiz ? {...aprendiz, ...datosAprendiz} :
+            aprendiz
+        })
     })
 })
 
-//Actualizar aprendiz 
-listaAprendices = listaAprendices.map(aprendiz => {
-    return aprendiz.di === diAprendiz ? {...aprendiz, ...datosAprendiz} :
-    aprendiz 
-
-})
 
 
-app.listen(puerto,()=>{
+
+
+app.listen(puerto, () => {
     console.log(`SERVIDOR http://localhost:${puerto}`)
 })
